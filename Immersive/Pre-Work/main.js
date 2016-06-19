4f8367c44566ddbe398409b03ccc77b39b0a44a9
Christopher Lopez <chrisa_lopez@yahoo.com>
2016-06-19 00:53:20 -0700

    Chapter 9

--- Immersive/Pre-Work/main.js
+++ Immersive/Pre-Work/main.js
@@ -3,3 +3,17 @@ var cardOne = "queen"
 var cardTwo = "king"
 var cardThree = "king"
 var cardFour = "queen"
+
+if (cardTwo === cardThree) {
+  alert ('You found a Match!')
+} else if (cardTwo !== cardOne ) {
+  alert ('Sorry, try again.')
+} else if (cardTwo !== cardFour) {
+  alert ('Sorry, try again.')
+} else if (cardOne !== cardThree) {
+  alert ('Sorry, try again.')
+} else if (cardThree !== cardFour) {
+  alert ('Sorry, try again.')
+} else (cardOne === cardFour) {
+  alert ('You found a Match!')
+}
